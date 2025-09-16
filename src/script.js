7a6cc5cb8285d548f15a11a1adbd3d0e49cd4216
Mbali Sithebe <2567890@students.wits.ac.za>
2025-09-16 11:57:39 +0200

    API

---
+++ src/script.js
@@ -0,0 +1,7 @@
+//API IMPLEMENTATION CODE EXAMPLE: 
+
+//1. Calling the APIs
+let charactersApiUrl  = 'https://thesimpsonsapi.com/api/characters'; 
+let episodesApiUrl = "https://thesimpsonsapi.com/api/episodes";
+
+
